26c80b6a35f446a73acbe20e9414b2b0345b3825
Devin Naicker <devinnaicker@gmail.com>
2024-11-17 11:30:08 -0800

    storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -14,9 +15,10 @@ class BaseModel:
                 elif key != '__class__':
                     self.__dict__[key] = kwargs[key]
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        storage.new(self)
 
     def __str__(self):
         """Formats the class and returns the formatted string"""
@@ -25,6 +27,7 @@ class BaseModel:
     def save(self):
         """Updates the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
